bff8a7a838c506e0697299763971c807a9a2dbcf
mf4TN <75338199+mf4TN@users.noreply.github.com>
2021-12-18 17:07:04 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,16 +1,14 @@
-echo " installing ... "
-echo " package Git installing ..."
-pkg install git 
-pkg install clone
+echo "Installing Packages ... "
+pkg install git clone
 clear
-echo " Git Installed ..."
+echo "Installing Tool ... "
 git clone  https://github.com/mf4TN/Combo-Dumper.py.git
 clear
-echo " installing Python ..."
+echo "installing Python ..."
 pkg install python
 clear
-echo " Done Install Packages "
-echo " Running Tool ..."
+echo "Done Installed All Packages "
+echo "Running Tool ..."
 mv Combo-Dumper.py combo_maker
 cd combo_maker 
 clear
